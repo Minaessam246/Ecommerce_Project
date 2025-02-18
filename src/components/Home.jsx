6ffe38d9e3cf0b5f77517f444cd7bd744861a58c
Minaessam246 <168353493+Minaessam246@users.noreply.github.com>
2025-02-18 18:00:14 +0200

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -163,6 +163,11 @@ setpreded(res.data.data)
 ))
 
 }
+     <div className='flex justify-center search items-center'>
+<h2 className={`text-3xl  font-bold   text-black capitalize ${filteredProducts.length==0?"":"hidden"}`}> no mathcing products
+
+</h2>
+</div>
 
 
     </div>
